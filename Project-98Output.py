1e62a659181775d96928852b446ed9fb1e335c11
Krishu-art <75996418+Krishu-art@users.noreply.github.com>
2021-12-07 19:13:59 +0530

    Add files via upload

---
+++ Project-98Output.py
@@ -0,0 +1,8 @@
+Python 3.8.6 (v3.8.6:db455296be, Sep 23 2020, 13:31:39) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /Users/krishu/Documents/SwappingFile.py ===============
+Give the File Name:-File1.txt
+Give the File Name:-File2.txt
+>>> 
